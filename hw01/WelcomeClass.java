0633d7ac4b8c323c0fe79939ace9ac13057b97ca
mrb218 <mrb218@lehigh.edu>
2014-08-29 15:36:22 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+///////////////////////
+//Mackenzie Burns
+//Hw 01
+//Welcome Class Java Program
+//
+//  define a class
+public class WelcomeClass{
+
+//  add main method
+  public static void main(String[] args) {
+ 
+//  print the statement with box around it
+    System.out.println("  -----------       ");
+    System.out.println("  | WELCOME |       ");
+    System.out.println("  -----------       ");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-m--r--b--2--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+  
+  }
+}
