983917af74b0d1b46dd68e0fccf4f034c6d80c65
mrb218 <mrb218@lehigh.edu>
2014-09-21 22:42:30 +0000

    updated Month

--- hw04/Month.java
+++ hw04/Month.java
@@ -18,20 +18,19 @@ public class Month {
         
         System.out.print("Enter an int giving the number of the month (1-12)");
         monthnumber = myScanner.nextInt();
-        if (monthnumber = 1 || monthnumber = 3 || monthnumber = 5 || monthnumber = 7 || monthnumber = 8 || monthnumber = 10 || monthnumber = 12) {
+        if (monthnumber == 1 || monthnumber == 3 || monthnumber == 5 || monthnumber == 7 || monthnumber == 8 || monthnumber == 10 || monthnumber == 12) {
             System.out.println("The month has 31 days.");
-        } else if (monthnumber = 4 || monthnumber = 6 || monthnumber = 9 || monthnumber = 11) {
+        } else if (monthnumber == 4 || monthnumber == 6 || monthnumber == 9 || monthnumber == 11) {
             System.out.println("The month has 30 days.");
-        } else if (monthnumber = 2) {
+        } else if (monthnumber == 2) {
             System.out.print("Enter an int giving the year:");
                 year = myScanner.nextInt();
                  
-                if ( year ) {
+                if (year == 1980 || year == 1984 || year == 1988 || year == 1992 || year == 1996 || year == 2000 || year == 2004 || year == 2008 || year == 2012) {
                     System.out.println("The month has 29 days.");
-                } if else (noLeapYear) {
-                    System.out.println("The month has 28 days.");
                 } else {
-                    System.out.println("You did not enter a positive int.");
+                    System.out.println("The month has 28 days.");
+              
         }
                 
         }
