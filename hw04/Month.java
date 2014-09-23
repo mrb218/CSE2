09fb885d8aa3aef375cc8836758eadb2995ee224
mrb218 <mrb218@lehigh.edu>
2014-09-23 03:41:58 +0000

    updated Month

--- hw04/Month.java
+++ hw04/Month.java
@@ -14,20 +14,25 @@ public class Month {
         Scanner myScanner;
         myScanner = new Scanner (System.in);
         
-        int year, monthnumber, LeapYear;
-        
+        int year, monthnumber, LeapYear; //determining ints
+        //asking you to put in a month
         System.out.print("Enter an int giving the number of the month (1-12)");
         monthnumber = myScanner.nextInt();
+        
+        //months with 31 days
         if (monthnumber == 1 || monthnumber == 3 || monthnumber == 5 || monthnumber == 7 || monthnumber == 8 || monthnumber == 10 || monthnumber == 12) {
             System.out.println("The month has 31 days.");
+            //months with 30 days
         } else if (monthnumber == 4 || monthnumber == 6 || monthnumber == 9 || monthnumber == 11) {
             System.out.println("The month has 30 days.");
+            //febuary has 28/29 days depending on the year
         } else if (monthnumber == 2) {
             System.out.print("Enter an int giving the year:");
                 year = myScanner.nextInt();
-                 
-                if (year == 1980 || year == 1984 || year == 1988 || year == 1992 || year == 1996 || year == 2000 || year == 2004 || year == 2008 || year == 2012) {
+                 //if it is a leap year, it has 29 days.
+                if (year % 4 == 0) {
                     System.out.println("The month has 29 days.");
+                    //if it is not a leap year, it has 28 days
                 } else {
                     System.out.println("The month has 28 days.");
               
