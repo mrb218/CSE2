87c9bc3f91ac925b6b9bee0bf8e04b5ef4b0eee7
mrb218 <mrb218@lehigh.edu>
2014-10-01 02:14:43 +0000

    updated BurgerKing

--- hw05/BurgerKing.java
+++ hw05/BurgerKing.java
@@ -18,83 +18,83 @@ public class BurgerKing {
     
     
     
-    
+    //asking user to enter a letter for the type of food they want
 System.out.print("Enter a letter to indicate a choice of Burger (B or b), Soda (S or s), or Fries (F or f): ");
     String food=myScanner.next();
     
     switch(food){
             case "B":
-            case "b":{
+            case "b":{ //if they want a burger, they can enter a letter for the topping
                 System.out.print("Enter a letter to indicate the topping- all the fixins (A or a), cheese (C or c), none of the aboce (N or n).");
                 String topping=myScanner.next();
                 switch(topping){
                     case "A":
-                    case "a": {
+                    case "a": { //if they want all the fixins on their burger
                         System.out.println("You ordered a burger with 'all the fixins'.");
                         }
                         break;
                     case "C":
-                    case "c":{
+                    case "c":{ //if they want a burger with cheese
                          System.out.println("You ordered a burger with cheese.");
                         }
                         break;
                     case "N":
-                    case "n": {
+                    case "n": { //if they want a plain burger
                         System.out.println("You ordered a burger with no fixins.");
                         }
                         break;
-                    default: 
+                    default:  //if they did not enter a letter that is specified
                     System.out.println("You did not enter any of B, b, S, s, F, or f.");
                 }
-                
+                //they can enter a letter if they want another type of food
             System.out.print("Enter a letter to indicate a choice of Burger (B or b), Soda (S or s), or Fries (F or f): ");
    
             }
     }
         switch(food){    
             case "S":
-            case "s": {
+            case "s": { //if they want a soda, they can enter a letter for the type of soda
                 System.out.print("Do you want a Pepsi (P or p), Coke (C or c), Sprite (S or s), or Mountain Dew (M or m)?");
                 String soda=myScanner.next();
                 switch(soda){
                     case "P":
-                    case "p":{
+                    case "p":{ //if they ordered a pepsi
                         System.out.println("You ordered a Pepsi.");
                         }
                         break;
                     case "C":
-                    case "c": {
+                    case "c": { //if they ordered a coke
                         System.out.println("You ordered a Coke.");
                         }
                         break;
                     case "S":
-                    case "s":{
+                    case "s":{ //if they ordered a sprite
                         System.out.println("You ordered a Sprite.");
                         }
                         break;
                     case "M":
-                    case "m": {    
+                    case "m": {    //if they ordered a mountain dew
                         System.out.println("You ordered a Mountain Dew.");
                         }
                         break;
-                    }
+                    } //they can enter a letter if they want another type of food
                 System.out.print("Enter a letter to indicate a choice of Burger (B or b), Soda (S or s), or Fries (F or f): ");
     
             }
         }
             switch(food){
             case "F":
-            case "f": {
+            case "f": { //if they ordered fries, they can choose before a large or small
                 System.out.print("Do you want a large or small order of fries (L, l, S, or s)?");
                 String fries=myScanner.next();
                 switch(fries){
                     case "L":
-                    case "l":{
+                    case "l":{ //if you ordered a large fries
                         System.out.println("You ordered large fries.");
                         }
                         break;
                     case "S":
-                    case "s":{
+                    case "s":{ //if you ordered a small fries
                         System.out.println("You ordered small fries.");
                         }
                         break;
@@ -104,7 +104,7 @@ System.out.print("Enter a letter to indicate a choice of Burger (B or b), Soda (
             
             
             default: 
-            
+            //if they did not enter a specified letter
             System.out.println("You did not enter any of B, b, S, s, F, or f.");
             
             }
