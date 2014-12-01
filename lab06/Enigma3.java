ad09461634a4d7c5ba4dc4c84309dcd75f705df2
mrb218 <mrb218@lehigh.edu>
2014-12-01 20:10:33 +0000

    Updated Enigma

--- lab06/Enigma3.java
+++ lab06/Enigma3.java
@@ -20,44 +20,62 @@ public class Enigma3{
       default: out+=40;
           n/=3;
           k-=7;
+          System.out.println("k at line 19: "+k);
+          System.out.println("n at line 20: "+n);
     }
     if(n*k%12< 12){
       n-=20;
+      System.out.println("k at line 24: "+k);
+      System.out.println("n at line 25: "+n);
       out+=n;
     }
     if(n*n>k){
       n=42;
       out+=n+k;
+      System.out.println("k at line 31: "+k);
+      System.out.println("n at line 32: "+n);
     }
     else {
       n=45;
       out+=n+k;
+      System.out.println("k at line 37: "+k);
+      System.out.println("n at line 38: "+n);
     }
     switch(n+k){
       case 114: 
         n-=11;
         k-=3;
+        System.out.println("k at line 44: "+k);
+        System.out.println("n at line 45: "+n);
         break;
       case 97:
         n-=14;
         k-=2;
+        System.out.println("k at line 50: "+k);
+        System.out.println("n at line 51: "+n);
         break;
       case 98:
         n/=5;
-        k/=9;
+        k/=9; 
+        System.out.println("k at line 56: "+k);
+        System.out.println("n at line 57: "+n);
       default:
         n-=3;
-        k-=5;
+        k=5; //error
+        System.out.println("k at line 61: "+k);
+        System.out.println("n at line 62: "+n);
     }
-     out+=1/n + 1/k;   
-    System.out.println(out);
+     out+=1/n + 1/k;  
+     System.out.println("k at line 65: "+k);
+     System.out.println("n at line 66: "+n);
+     System.out.println(out);
   }
 }
 
 /*
  * Error report:
- * 
- * 
+ * i put print statements in each of the cases
+ * needed to run without errors so changed line 64
  * 
  * 
  */
