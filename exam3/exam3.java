8f359a41dbc48c688f3bf85046107f6830188ccc
mrb218 <mrb218@lehigh.edu>
2014-12-01 19:41:17 +0000

    Updated Enigma1

---
+++ exam3/exam3.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class PokerOdds{
+public static  int enigma(double x[], double []y, int z[]){
+    int x[] = {1, 2, 4, 6};
+    double [] u ={3,4,5,6,7}, v={2,4,5};
+    
+    double []temp;
+    temp =x;
+    temp[z[2]]=42.0;
+    y=x;
+    y[0]=2.34;
+    return z[z[0]];
+    
+    System.out.println(enigma(u,v,x));
+    
+    }
+
+}
--- lab06/Enigma1.java
+++ lab06/Enigma1.java
@@ -7,23 +7,38 @@
 import java.util.Scanner;
 public class Enigma1{
   public static void main(String []arg){
+    
     double percent;
     System.out.print("Enter a value for the percent (0, 1,...99)- ");
-    double x=((new Scanner(System.in)).nextDouble());
+    Scanner scan = new Scanner(System.in);
+    
+    double x= scan.nextDouble();
    System.out.println("You entered "+x+"%");
    //print out the proportion remaining for select percentages
-   percent = x/100;
-   if(1-percent==0.93)  //when the user enters 7
+   double y = 1-(x/100);
+   double z = Math.round(y*100.0)/100.0;
+   
+   if(z==0.93) { //when the user enters 7
       System.out.println("The proportion remaining is "+0.93);
-    else if(1-percent==0.59)//when the user enters 41
+  }
+    else if(z==0.59){//when the user enters 41
       System.out.println("The proportion remaining is "+0.59);
-    else if(1-percent==0.86)//when the user enters 14
+  }
+    else if(z==0.86){//when the user enters 14
       System.out.println("The proportion remaining is "+0.86);
-    else if(1-percent==0.67)//when the user enters 33
+  }
+    else if(z==0.67){//when the user enters 33
       System.out.println("The proportion remaining is "+0.67);
-    else if(1-percent==0.4)//when the user enters 60
+  }
+    else if(z==0.4){//when the user enters 60
       System.out.println("The proportion remaining is "+0.40);
-   }
+  }
+    else{
+      System.out.println("You did not enter one of the correct input numbers.");
+      return;
+    }
+    
+  }
 }
 
 /* Error report: 
@@ -31,6 +46,8 @@ public class Enigma1{
  *    and fix the errors)
  *    Hint: What kinds of input are unacceptable? What kinds of
  *        acceptable input don't produce the correct answer?
+ *  Need to change the scanner so that the input can actually be used
+ *  It only works for the numbers 7, 41, 14, 33, and 60 so it is not very reliable, it should have an else statement
  * the percent was not initialized- you should determine percent before the
  * if statement.
  * 
